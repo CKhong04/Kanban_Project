8fbc4cbaa3d1e5e3636aacabe096c5d8aa8d677b
milnia4 <mabe0012@student.monash.edu>
2023-09-08 11:52:31 +1000

    integerated all files with flask

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 EMC & Co.
 
 Youtube link ref: https://www.youtube.com/watch?v=G0jO8kUrg-I&ab_channel=GreatStack  
+Youtube link ref for Flask : https://www.youtube.com/watch?v=GHvj1ivQ7ms
---
+++ static/js/script.js
@@ -0,0 +1,47 @@
+const inputBox= document.getElementById("input-box");
+const listContainer= document.getElementById("list-container");
+
+// adding a task
+function addTask(){
+    if(inputBox.value===''){
+        alert("Box cannot be empty!")
+    }
+    else{
+        let li= document.createElement("li");
+        li.innerHTML=inputBox.value;
+
+        // adds to the list of tasks with option to 'span' / close
+        listContainer.appendChild(li)
+        let span= document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value="";
+
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName==="LI"){
+
+        // toggle for checking and unchecking a box
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+        // toggle for deleting a box
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+// saving all the tasks onto local machine
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+// retrieving local stored data each time program is opened 
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data")
+}
+showTask();
