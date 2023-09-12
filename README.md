17a8c8dc370f3e239b5a57230e61ebd958f1ea5f
CKhong04 <136895775+CKhong04@users.noreply.github.com>
2023-09-13 09:39:32 +1000

    Added drag and drop functionality within the To-Do list column. Also credited the source in the README file.

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ EMC & Co.
 
 Youtube link ref: https://www.youtube.com/watch?v=G0jO8kUrg-I&ab_channel=GreatStack  
 Youtube link ref for Flask : https://www.youtube.com/watch?v=GHvj1ivQ7ms
+
+Reference for dragging and dropping functionality: https://www.codingnepalweb.com/drag-and-drop-sortable-list-html-javascript/
--- static/js/script.js
+++ static/js/script.js
@@ -3,74 +3,101 @@ const listContainer= document.getElementById("list-container");
 
 // adding a task
 function addTask(){
-    if(inputBox.value===''){
-        alert("Box cannot be empty!")
-    }
-    else{
-        let li= document.createElement("li");
-        li.innerHTML=inputBox.value;
+if(inputBox.value===''){
+alert("Box cannot be empty!")
+}
+else{
+let li= document.createElement("li");
+li.innerHTML=inputBox.value;
 
-        // adds to the list of tasks with option to 'span' / close
-        listContainer.appendChild(li)
-        let span= document.createElement("span");
-        let span2= document.createElement("span2");
-        let span3= document.createElement("span3");
-        span.innerHTML="\u00d7";
-        span2.innerHTML="\uD83E\uDC61"; //up arrow utf-16
-        span3.innerHTML="\uD83E\uDC63"; //down arrow utf-16
-        li.appendChild(span);
-        li.appendChild(span2);
-        li.appendChild(span3);
-    
-    }
-    inputBox.value="";
+// adds to the list of tasks with option to 'span' / close
+listContainer.appendChild(li)
+let span= document.createElement("span");
+let span2= document.createElement("span2");
+let span3= document.createElement("span3");
+span.innerHTML="\u00d7";
+span2.innerHTML="\uD83E\uDC61"; //up arrow utf-16
+span3.innerHTML="\uD83E\uDC63"; //down arrow utf-16
+li.appendChild(span);
+li.appendChild(span2);
+li.appendChild(span3);
 
-    saveData();
+}
+inputBox.value="";
+
+saveData();
 }
 
 listContainer.addEventListener("click", function(e){
-    if(e.target.tagName==="LI"){
+if(e.target.tagName==="LI"){
 
-        // toggle for checking and unchecking a box
-        e.target.classList.toggle("checked");
-        saveData();
-    }
-        // toggle for deleting a box
-    else if(e.target.tagName==="SPAN"){
-        e.target.parentElement.remove();
-        saveData();
-    } else if (e.target.tagName === "SPAN2") {
-        // Find the parent LI element
-        const li = e.target.parentElement;
-        
-        // Check if it's not the first element
-        if (li.previousElementSibling) {
-            // Move the LI element up by inserting it before the previous sibling
-            li.parentElement.insertBefore(li, li.previousElementSibling);
-            saveData();
-        }
-    } else if (e.target.tagName === "SPAN3") {
-        // Find the parent LI element
-        const li = e.target.parentElement;
-        
-        // Check if it's not the last element
-        if (li.nextElementSibling) {
-            // Move the LI element down by inserting it after the next sibling
-            li.parentElement.insertBefore(li.nextElementSibling, li);
-            saveData();
-        }
-    }
+// toggle for checking and unchecking a box
+e.target.classList.toggle("checked");
+saveData();
+}
+// toggle for deleting a box
+else if(e.target.tagName==="SPAN"){
+e.target.parentElement.remove();
+saveData();
+} else if (e.target.tagName === "SPAN2") {
+// Find the parent LI element
+const li = e.target.parentElement;
+
+// Check if it's not the first element
+if (li.previousElementSibling) {
+// Move the LI element up by inserting it before the previous sibling
+li.parentElement.insertBefore(li, li.previousElementSibling);
+saveData();
+}
+} else if (e.target.tagName === "SPAN3") {
+// Find the parent LI element
+const li = e.target.parentElement;
+
+// Check if it's not the last element
+if (li.nextElementSibling) {
+// Move the LI element down by inserting it after the next sibling
+li.parentElement.insertBefore(li.nextElementSibling, li);
+saveData();
+}
+}
 }, false);
 
 // saving all the tasks onto local machine
 function saveData(){
-    localStorage.setItem("data", listContainer.innerHTML);
+localStorage.setItem("data", listContainer.innerHTML);
 }
 
-// retrieving local stored data each time program is opened 
+// retrieving local stored data each time program is opened
 function showTask(){
-    listContainer.innerHTML=localStorage.getItem("data")
+listContainer.innerHTML=localStorage.getItem("data")
 }
+
+//allowing the user to drag and drop different items
+const sortableList = document.querySelector(".sortable-list");
+const items = sortableList.querySelectorAll(".item");
+items.forEach(item => {
+item.addEventListener("dragstart", () => {
+// Adding dragging class to item after a delay
+    setTimeout(() => item.classList.add("dragging"), 0);
+});
+// Removing dragging class from item on dragend event
+item.addEventListener("dragend", () => item.classList.remove("dragging"));
+});
+const initSortableList = (e) => {
+    e.preventDefault();
+    const draggingItem = document.querySelector(".dragging");
+    // Getting all items except currently dragging and making array of them
+    let siblings = [...sortableList.querySelectorAll(".item:not(.dragging)")];
+    // Finding the sibling after which the dragging item should be placed
+    let nextSibling = siblings.find(sibling => {
+        return e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2;
+    });
+    // Inserting the dragging item before the found sibling
+    sortableList.insertBefore(draggingItem, nextSibling);
+}
+sortableList.addEventListener("dragover", initSortableList);
+sortableList.addEventListener("dragenter", e => e.preventDefault());
+
 showTask();
 
 checkLogin();
