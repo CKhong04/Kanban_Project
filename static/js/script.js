f2ea3a347911279b8edd3b30c5aedaab06289ea7
Carissa Khong <ckho0027@student.monash.edu>
2023-10-18 20:11:49 +1100

    Added documentation and commenting for various sections of the code within the index.html and script.js files.

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', () => {
-	todos = JSON.parse(localStorage.getItem('todos')) || [];
+	todos = JSON.parse(localStorage.getItem('todos')) || []; //Create local storage for all the tasks, allowing the page to be refreshed and for the content to stay in its spot on the page.
 	const nameInput = document.querySelector('#name');
 	const newTodoForm = document.querySelector('#new-todo-form');
 
@@ -7,11 +7,11 @@ window.addEventListener('load', () => {
 
 	nameInput.value = username;
 
-	nameInput.addEventListener('change', (e) => {
+	nameInput.addEventListener('change', (e) => { //When the name in the form is changed, the updated value will be stored in local storage.
 		localStorage.setItem('username', e.target.value);
 	})
 
-	newTodoForm.addEventListener('submit', e => {
+	newTodoForm.addEventListener('submit', e => { //When the form is submitted, a task is added to the local storage (as a todo).
 		e.preventDefault();
 
 		const todo = {
@@ -20,7 +20,7 @@ window.addEventListener('load', () => {
 			personcontent: e.target.elements.personcontent.value,
 			done: false,
 			createdAt: new Date().getTime(),
-			status: 'backlog'
+			status: 'backlog' //The status is set to 'backlog', important for allowing refreshing of the page
 		}
 
 		todos.push(todo);
@@ -37,16 +37,12 @@ window.addEventListener('load', () => {
 })
 
 function DisplayTodos () {
-	const todoList = document.getElementById('todo-list');
-	const todoLane = document.getElementById("todo-lane");
-	const doingLane = document.getElementById("doing-lance");
-	const doneLane = document.getElementById("done-lane");
-	const categories = new Set(['ui', 'ux', 'front-end', 'back-end', 'database', 'api', 'framework'])
+	const todoList = document.getElementById('todo-list'); //This is the product backlog section
+	const todoLane = document.getElementById("todo-lane"); //This is the to-do lane in the sprint backlog
+	const categories = new Set(['ui', 'ux', 'front-end', 'back-end', 'database', 'api', 'framework']) //Create a new set of seven of the eight tags available.
     
 	todoList.innerHTML = "";
-	[...todoLane.children].filter(e => e.tagName.toLowerCase() === 'div').forEach(e => e.remove());
-	[...todoLane.children].filter(e => e.tagName.toLowerCase() === 'div').forEach(e => e.remove());
-    
+	[...todoLane.children].filter(e => e.tagName.toLowerCase() === 'div').forEach(e => e.remove()); //Check for any children in the todo lane that are part of another 'div' element. If these are present, remove them (this prevents double ups in the system.)
 
 	todos.forEach(todo => {
 		const todoItem = document.createElement('div');
@@ -62,7 +58,7 @@ function DisplayTodos () {
 			todoItem.classList.remove("is-dragging");
 		});
 
-
+		//Create the various elements that are available within the tasks in the product backlog and sprint backlog.
 		const label = document.createElement('label');
 		const input = document.createElement('input');
 		const span = document.createElement('span');
@@ -76,47 +72,50 @@ function DisplayTodos () {
 		input.type = 'checkbox';
 		input.checked = todo.done;
 		span.classList.add('bubble');
-		span.classList.add(categories.has(todo.category) ? todo.category : 'testing')
+		span.classList.add(categories.has(todo.category) ? todo.category : 'testing') //Sets the tag to testing if no tag is given when the task is added to the backlog.
 		content.classList.add('todo-content');
 		actions.classList.add('actions');
 		deleteButton.classList.add('delete');
 		addToSprintButton.classList.add('add');
 		removeFromSprintButton.classList.add('removeSprint');
 
-		content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
+		//Add the text which appears in the buttons given below.
+		content.innerHTML = `<input type="text" value="${todo.content}" readonly>`; //Set the content in the tasks to be read-only
 		deleteButton.innerHTML = 'Delete';
 		addToSprintButton.innerHTML = 'Add to Sprint';
 		removeFromSprintButton.innerHTML = 'Remove from Sprint';
 
-
+		//Add the various elements of a task into the todoItem, which represents the task.
 		label.appendChild(input);
 		label.appendChild(span);
 		actions.appendChild(deleteButton);
 		actions.appendChild(addToSprintButton);
 		todoItem.appendChild(label);;
 		todoItem.appendChild(content);
+		todoItem.appendChild(actions);
 
 		// Display 'who is doing this task' on the task tile
 		const personContent = document.createElement('p');
 		personContent.innerHTML =  `<input type="text" value="${todo.personcontent}" readonly>`; 
 		todoItem.appendChild(personContent);
-		
-		todoItem.appendChild(actions);
 
+		//When a task has the status 'backlog', it is in the product backlog. This means it should be present in the product backlog list.
 		if (todo.status === 'backlog') {
 			todoList.appendChild(todoItem)
+		//When a task has status 'todo', it has been added to the sprint backlog.
 		} else if (todo.status === 'todo') {
 			todoItem.style.width = "100%";
 			todoItem.style.display = "flex";
 			span.style.marginTop = "12px";
-			todoItem.removeChild(actions);
-			todoItem.appendChild(removeFromSprintButton);
-			todoLane.appendChild(todoItem);
+			todoItem.removeChild(actions); //The actions are not important in the sprint backlog, therefore they are removed.
+			todoItem.appendChild(removeFromSprintButton); //Add the 'remove from sprint' button to the item.
+			todoLane.appendChild(todoItem); //Add the item to the sprint backlog's todo lane.
 		}
 
-		if (todo.done) {
-			todoItem.classList.add('done');
-			let number = Math.random() * 3;
+		if (todo.done) { //Runs when a task is marked off as 'done'.
+			todoItem.classList.add('done'); //Add the status 'done' to the task, allowing it to take on special properties.
+			let number = Math.random() * 3; //Generate a random number between 0 and 3.
+			//Three possible alerts are created, which will be chosen at random to display to the user.
 			if (number <= 1){
 				alert("Well done on completing a task! You are awesome!");
 			} else if (number <= 2){
@@ -126,6 +125,7 @@ function DisplayTodos () {
 			}
 		}
 		
+		//Switch the status of the task from 'done' and back, when the input to the task's bubble is changed.
 		input.addEventListener('change', (e) => {
 			todo.done = e.target.checked;
 			localStorage.setItem('todos', JSON.stringify(todos));
@@ -140,6 +140,7 @@ function DisplayTodos () {
 
 		})
 
+		//When the content of the tasks is double clicked, allow it to be edited.
 		content.addEventListener('dblclick', (e) => {
 			const input = content.querySelector('input');
 			input.removeAttribute('readonly');
@@ -153,6 +154,7 @@ function DisplayTodos () {
 			})
 		})
 
+		//When the person's name is double clicked, allow it to be edited.
 		personContent.addEventListener('dblclick', (e) => {
 			const input = personContent.querySelector('input');
 			input.removeAttribute('readonly');
@@ -165,24 +167,21 @@ function DisplayTodos () {
 			});
 		});
 
+		//When the delete button is clicked, remove it from the list and accordingly adjust the locally stored tasks.
 		deleteButton.addEventListener('click', (e) => {
 			todos = todos.filter(t => t !== todo);
 			localStorage.setItem('todos', JSON.stringify(todos));
 			DisplayTodos()
 		})
 
+		//When the add to sprint button is clicked, change the status of the task to 'todo', then redisplay all the tasks.
 		addToSprintButton.addEventListener('click', (e) => {
-			todoItem.style.width = "100%";
-			todoItem.style.display = "flex";
-			span.style.marginTop = "12px";
-			todoItem.removeChild(actions);
-			todoItem.appendChild(removeFromSprintButton);
-			todoLane.appendChild(todoItem);
 			todo.status = 'todo'
 			localStorage.setItem('todos', JSON.stringify(todos));
 			DisplayTodos()
 		})
 
+		//When the remove from sprint button is clicked, add the item back to the product backlog.
 		removeFromSprintButton.addEventListener('click', (e) => {
 			todoItem.appendChild(actions);
 			todoItem.removeChild(removeFromSprintButton);
@@ -195,9 +194,10 @@ function DisplayTodos () {
 	})
 }
 
-const draggables = document.querySelectorAll(".tasks");
-const droppables = document.querySelectorAll(".swim-lane");
+const draggables = document.querySelectorAll(".tasks"); //Set all tasks to be draggable
+const droppables = document.querySelectorAll(".swim-lane"); //Set all swim lanes to be drop zones
 
+//For each draggable object, add event listeners which check whether the object is being dragged or dropped.
 draggables.forEach((tasks) => {
   tasks.setAttribute("draggable", "true")
   tasks.addEventListener("dragstart", () => {
@@ -208,6 +208,7 @@ draggables.forEach((tasks) => {
   });
 });
 
+//Allow all drop zones to be dragged over and have draggable objects appended.
 droppables.forEach((zone) => {
   zone.addEventListener("dragover", (e) => {
     e.preventDefault();
@@ -222,9 +223,11 @@ droppables.forEach((zone) => {
     }
   });
 
+  //When a draggable object enters the drop zone, stop the default code from occurring.
   zone.addEventListener("dragenter", e => e.preventDefault());
 });
 
+//Allow the task being dragged to find its closest task below so it can be dropped there.
 const insertAboveTask = (zone, mouseY) => {
   const els = zone.querySelectorAll(".tasks:not(.is-dragging)");
 
